a3babd0049d57f857b80082dfd9ad16abd15c704
jchoi071 <jchoi071@ucr.edu>
2019-03-17 22:47:13 -0700

    Update led_matrix.c

--- Custom Lab/Tetris/Tetris/led_matrix.c
+++ Custom Lab/Tetris/Tetris/led_matrix.c
@@ -11,10 +11,13 @@
 
 unsigned char screen[8];
 
+// UCR-provided set bit function
 unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b)
 {
     return (b ? x | (0x01 << k) : x & ~(0x01 << k));
 }
+
+// UCR-provided get bit function
 unsigned char GetBit(unsigned char x, unsigned char k)
 {
     return ((x & (0x01 << k)) != 0);
